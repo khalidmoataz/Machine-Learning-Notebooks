d3d88c12a413ce824aba1d2dbeae029ee2ea9313
khalid moataz <khalidmoataz5@gmail.com>
2021-05-17 13:41:33 +0200

    Add new files

---
+++ Computer Vision Codes/Computer Vision/Ben Graham's.py
@@ -0,0 +1,8 @@
+def load_ben_color(path, sigmaX=10):
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = crop_image_from_gray(image)
+    image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
+    image=cv2.addWeighted ( image,4, cv2.GaussianBlur( image , (0,0) , sigmaX) ,-4 ,128)
+        
+    return image
